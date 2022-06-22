c21d18497ddae3893cf35ed849e632b059a1a02c
Phuc Nguyen <phucnt13@gmail.com>
2022-06-22 13:28:23 +0700

    first commit

---
+++ src/main/java/phuc/dev/bemoneytracker/BeMoneyTrackerApplication.java
@@ -0,0 +1,13 @@
+package phuc.dev.bemoneytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeMoneyTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeMoneyTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/phuc/dev/bemoneytracker/BeMoneyTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package phuc.dev.bemoneytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeMoneyTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
